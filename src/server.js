777de718f2fb84e4310ca1f68e5f5d93be723a3d
Jovohir <java6923@mail.ru>
2025-07-25 09:29:03 +0500

    up

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from 'express'
 
-import { configServer } from './config/server.config.js'
 import { connectDB } from './db/index.js'
 import router from './routes/index.route.js'
 import cookieParse from 'cookie-parser'
+import { configServer } from './config/server.config.js'
 await connectDB()
+
 const server = express();
 server.use(express.json())
 server.use(cookieParse())
 server.use('/api', router)
 
-const PORT = +configServer.PORT || 2000
+const PORT = configServer.PORT
 
 server.listen(PORT, () => console.log('Server is running PORT:', PORT))
